a3f906a3e809cd46ca017ca0c000f9174e2bd605
Rafael Fernandes Lopes <rafael.fernandes@gmail.com>
2020-11-18 22:21:30 -0300

    Implementado um Drawer simples

--- lib/core/main_screen.dart
+++ lib/core/main_screen.dart
@@ -7,6 +7,33 @@ class MainScreen extends StatelessWidget {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://diariodorio.com/wp-content/uploads/2020/07/daleopizzaria_20200710_144435_0.jpg"),
+                ),
+                decoration: BoxDecoration(
+                  color: Color(0xFF732128),
+                ),
+              ),
+              ListTile(
+                title: Text("Opção 1"),
+                onTap: () => true,
+              ),
+              ListTile(
+                title: Text("Opção 2"),
+                onTap: () => true,
+              ),
+              ListTile(
+                title: Text("Opção 3"),
+                onTap: () => true,
+              ),
+            ],
+          ),
+        ),
         appBar: AppBar(
           title: Text("Restaurante"),
           backgroundColor: Color(0xFF732128),
